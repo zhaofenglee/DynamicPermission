9638bfe773f191bbdb3ccd9d9e5ab23bdbf13b9c
zhaofenglee <zhaof_li@163.com>
2024-04-25 19:00:40 +0800

    Upgrade Abp to 8.1.0.

--- host/JS.Abp.DynamicPermission.Blazor.Server.Host/Migrations/20240425013830_update_abp8.1.0.Designer.cs
+++ host/JS.Abp.DynamicPermission.Blazor.Server.Host/Migrations/20240425013830_update_abp8.1.0.Designer.cs
@@ -4,6 +4,7 @@ using JS.Abp.DynamicPermission.Blazor.Server.Host.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace JS.Abp.DynamicPermission.Blazor.Server.Host.Migrations
 {
     [DbContext(typeof(UnifiedDbContext))]
-    partial class UnifiedDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240425013830_update_abp8.1.0")]
+    partial class update_abp810
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
